216a083bc2deb92aabb1556840ae943a03246b07
Maxim Kulkin <maxim.kulkin@gmail.com>
2020-04-20 16:18:59 -0700

    Fix spacing issue in custom_html code

--- src/wifi_config.c
+++ src/wifi_config.c
@@ -217,17 +217,17 @@ static void wifi_config_server_on_settings(client_t *client) {
     client_send_chunk(client, html_settings_header);
 
     if (context->custom_html != NULL && context->custom_html[0] > 0) {
-	uint8_t buffer_size = strlen(html_settings_custom_html) + strlen(context->custom_html); 
-	char* buffer = (char*) calloc(buffer_size, sizeof(char)); //fill up the buffer with zeros
-	snprintf(buffer, buffer_size, html_settings_custom_html, context->custom_html); //fill in template with the custom_html content
-	client_send_chunk(client, buffer); 
-	free(buffer);
+        uint8_t buffer_size = strlen(html_settings_custom_html) + strlen(context->custom_html); 
+        char* buffer = (char*) calloc(buffer_size, sizeof(char)); //fill up the buffer with zeros
+        snprintf(buffer, buffer_size, html_settings_custom_html, context->custom_html); //fill in template with the custom_html content
+        client_send_chunk(client, buffer);
+        free(buffer);
     }
 
     client_send_chunk(client, html_settings_body);
 
     if (xSemaphoreTake(wifi_networks_mutex, 5000 / portTICK_PERIOD_MS)) {
-	char buffer[64];
+        char buffer[64];
         wifi_network_info_t *net = wifi_networks;
         while (net) {
             snprintf(
